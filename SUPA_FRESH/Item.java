79265a9df909a9194736151334cf85f120c88e91
duartepbrandao <emduartepbrandao@hotmail.com/em>
2015-06-09 00:30:23 +0100

    fixed phd html

--- SUPA_FRESH/Item.java
+++ SUPA_FRESH/Item.java
@@ -1034,7 +1034,7 @@ public class Item{
 							+ " \n   <tbody>"
 							);
 							break;
-							
+
 							case 10: //manual
 							out.println("<!DOCTYPE html>"
 							+ "\n <html>"
@@ -1673,14 +1673,12 @@ public class Item{
 							+ " \n      <tr>"
 							+ " \n"
 							+ " \n        <th>BibtexKey</th>"
+							+ " \n        <th>Author</th>"
 							+ " \n        <th>Title</th>"
+							+ " \n        <th>School</th>"
 							+ " \n        <th>Year</th>"
-							+ " \n        <th>Editor</th>"
-							+ " \n        <th>Language</th>"
-							+ " \n        <th>Series</th>"
-							+ " \n        <th>Volume</th>"
-							+ " \n        <th>Number</th>"
-							+ " \n        <th>Organization</th>"
+							+ " \n        <th>Type</th>"
+							+ " \n        <th>Address</th>"
 							+ " \n        <th>Month</th>"
 							+ " \n        <th>Note</th>"
 							+ " \n        <th>URL</th>"
@@ -2155,18 +2153,18 @@ public class Item{
 			public String replaceSpecialCharacteres(String str)
 			{
 				//(not used) javac -encoding UTF8
-				str = str.replace("\\´a","á").replace("\\'a","à");
-				str = str.replace("\\~a","ã").replace("\\^a","â");
-				str = str.replace("\\'e","é").replace("\\^e","ê");
-				str = str.replace("\\´{\\i}","í").replace("\\´I","Í");
-				str = str.replace("\\´o","õ").replace("\\~o","Ãµ");
-				str = str.replace("\\^o","ô").replace("\\´u","ú");
-				str = str.replace("\\\"u","ü").replace("\\c{c}","ç"); 
-				str = str.replace("\\c{C}","Ç").replace("{\"}","\"");
-				str = str.replace("\\endash","–").replace("\\emdash","—");
+				str = str.replace("\\ï¿½a","ï¿½").replace("\\'a","ï¿½");
+				str = str.replace("\\~a","ï¿½").replace("\\^a","ï¿½");
+				str = str.replace("\\'e","ï¿½").replace("\\^e","ï¿½");
+				str = str.replace("\\ï¿½{\\i}","ï¿½").replace("\\ï¿½I","ï¿½");
+				str = str.replace("\\ï¿½o","ï¿½").replace("\\~o","Ãµ");
+				str = str.replace("\\^o","ï¿½").replace("\\ï¿½u","ï¿½");
+				str = str.replace("\\\"u","ï¿½").replace("\\c{c}","ï¿½");
+				str = str.replace("\\c{C}","ï¿½").replace("{\"}","\"");
+				str = str.replace("\\endash","ï¿½").replace("\\emdash","ï¿½");
 				str = str.replace("\\-","-").replace("\\#","#");
 				str = str.replace("\\&","&").replace("\\$","$");
-				str = str.replace("\\~n","ñ").replace("\\dots","...").replace("\\-","-");
+				str = str.replace("\\~n","ï¿½").replace("\\dots","...").replace("\\-","-");
 				return str;
 			}
 			/*
